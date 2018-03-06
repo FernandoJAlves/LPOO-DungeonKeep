6e0c288ac724f05ea8018e8d7dbf051404aa38b6
joao <joao-carlos.14@hotmail.com>
2018-03-06 18:56:07 +0000

    Even more bugs fixed

--- Dungeon Keep/src/dkeep/logic/Guard.java
+++ Dungeon Keep/src/dkeep/logic/Guard.java
@@ -12,55 +12,7 @@ public abstract class Guard extends Character{
 	
 	public abstract void updateIndex();
 	
-	public void move(char[][] map) {
-		char c = this.moves[this.index];
-		updateIndex();
-		
-		if(c == 'w'){
-			if(this.get_y() == 0) {
-				return;
-			}
-			char temp = map[this.get_y()-1][this.get_x()];
-			if(temp == 'X' || temp == 'I') {
-				return;
-			}
-			this.set_y(this.get_y() - 1);
-			
-		}
-		else if(c == 'd') {
-			if(this.get_x() == map[0].length-1) {
-				return;
-			}
-			char temp = map[this.get_y()][this.get_x() + 1];
-			if(temp == 'X' || temp == 'I') {
-				return;
-			}
-			this.set_x(this.get_x() + 1);
-			
-		}
-		else if(c == 's') {
-			if(this.get_y() == map.length-1) {
-				return;
-			}
-			char temp = map[this.get_y()+1][this.get_x()];
-			if(temp == 'X' || temp == 'I') {
-				return;
-			}
-			this.set_y(this.get_y() + 1);
-			
-		}
-		else if(c == 'a') {
-			if(this.get_x() == 0) {
-				return;
-			}
-			char temp = map[this.get_y()][this.get_x() - 1];
-			if(temp == 'X' || temp == 'I') {
-				return;
-			}
-			this.set_x(this.get_x() - 1);
-			
-		}
-	}
+	public abstract void move(char[][] map);
 
 
 
