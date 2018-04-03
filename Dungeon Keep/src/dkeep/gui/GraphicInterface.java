1d0158953988ef3034e2749f3eadb8b2bbe17b2a
joao <joao-carlos.14@hotmail.com>
2018-04-03 18:21:39 +0100

    Level editor bugs fixed

--- Dungeon Keep/src/dkeep/gui/GraphicInterface.java
+++ Dungeon Keep/src/dkeep/gui/GraphicInterface.java
@@ -4,8 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.concurrent.TimeUnit;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
