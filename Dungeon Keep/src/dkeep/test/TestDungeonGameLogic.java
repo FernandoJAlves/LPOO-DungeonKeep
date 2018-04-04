a977da4064ecc428e7908b58c271e89b8a59f700
Fernando Alves <up201605270@fe.up.pt>
2018-04-04 20:28:10 +0100

    More JUnit Tests (Custom Level)

--- Dungeon Keep/src/dkeep/test/TestDungeonGameLogic.java
+++ Dungeon Keep/src/dkeep/test/TestDungeonGameLogic.java
@@ -201,6 +201,181 @@ public class TestDungeonGameLogic{
 		}
 	}
 	
+	@Test(timeout=1000)
+	public void testSuspiciousInvertedMovement() {
+		
+		Game game = new Game();
+		game.initialize(1, 0, 2, null);
+		assertEquals(new Point(8,1), ((Level1)game.getMap()).getGuard().getPoint());
+		boolean inverted = false;
+		
+		//TODO
+		/*
+		while(!inverted) {
+			((Level1)game.getMap()).move_npc();
+			if(((Suspicious)(((Level1)game.getMap()).getGuard())).getInverted()) {
+				inverted = true;
+			}
+		}
+		
+		*/
+	}
+	
+	@Test
+	public void testOgreRandom() {
+		
+		Game game = new Game();
+		game.initialize(2, 1, 0, null);
+		
+		//TODO
+		/*
+		((Level2)game.getMap()).getOgres().get(0).set_x(4);
+		((Level2)game.getMap()).getOgres().get(0).set_y(3);
+		boolean expected_pos = true;
+		int counter = 0;
+
+		while (counter < 100) {
+			((Level2) game.getMap()).move_npc();
+
+			expected_pos = (((Level2) game.getMap()).getOgres().get(0).getPoint().equals(new Point(4, 2))
+					|| ((Level2) game.getMap()).getOgres().get(0).getPoint().equals(new Point(4, 4))
+					|| ((Level2) game.getMap()).getOgres().get(0).getPoint().equals(new Point(5, 3))
+					|| ((Level2) game.getMap()).getOgres().get(0).getPoint().equals(new Point(3, 3)));
+	
+			((Level2)game.getMap()).getOgres().get(0).set_x(4);
+			((Level2)game.getMap()).getOgres().get(0).set_y(3);
+			counter++;
+		}
+		
+		assertTrue(expected_pos);
+		*/
+	}
+	
+	
+	@Test
+	public void testOgreClubRandom() {
+		
+		Game game = new Game();
+		game.initialize(2, 1, 0, null);
+		
+		//TODO
+		/*
+		((Level2)game.getMap()).getOgres().get(0).set_x(4);
+		((Level2)game.getMap()).getOgres().get(0).set_y(3);
+		boolean expected_pos = true;
+		int counter = 0;
+
+		while (counter < 100) {
+			((Level2)game.getMap()).getOgres().get(0).club_logic(((Level2)game.getMap()).getMap(null));
+
+			expected_pos = (((Level2) game.getMap()).getOgres().get(0).club_hit.equals(new Point(4, 2))
+					|| ((Level2) game.getMap()).getOgres().get(0).club_hit.equals(new Point(4, 4))
+					|| ((Level2) game.getMap()).getOgres().get(0).club_hit.equals(new Point(5, 3))
+					|| ((Level2) game.getMap()).getOgres().get(0).club_hit.equals(new Point(3, 3)));
+
+			counter++;
+		}
+		
+		assertTrue(expected_pos);
+		*/
+	}
+	
+	@Test
+	public void testHeroIsCapturedByGuardLvl1() {
+		
+		Game game = new Game();
+		game.initialize(1, 0, 0, null);
+		((Level1)game.getMap()).getGuard().set_x(3);
+		((Level1)game.getMap()).getGuard().set_y(1);
+		game.setPosHero(1, 1);
+		assertFalse(game.isGameover());
+		game.moveHero('d'); // move hero to the right.
+		game.setState(game.collision());
+		assertTrue(game.isGameover());
+	}
+	
+	@Test
+	public void testHeroStunsOgreLvl2() {
+		
+		Game game = new Game();
+		game.initialize(2, 1, 0, null);
+		
+		//TODO
+		/*
+		((Level1)game.getMap()).getOgres().add(new Ogre(3,1));
+		game.setPosHero(1, 1);
+		assertFalse(game.isGameover());
+		game.moveHero('d'); // move hero to the right.
+		game.setState(game.collision());
+		assertFalse(game.isGameover());
+		assertTrue(((Level1)game.getMap()).ogres.get(0).isStunned());
+		assertEquals(2, ((Level1)game.getMap()).ogres.get(0).get_stun_count());
+		*/
+	}
+	
+	
+	
+	@Test
+	public void testHeroKeyInteractionCustomLvl() {
+		
+		Game game = new Game();
+		char[][] temp = {
+				{'X','X','X','X','X'},
+				{'X','H','k',' ','I'},
+				{'X',' ',' ',' ','X'},
+				{'X',' ',' ',' ','X'},
+				{'X','X','X','X','X'}
+		};
+		game.initialize(0, 0, 0, temp);
+		assertEquals(Game.Game_State.CLVL, game.state);
+		assertEquals('I', ((CustomLevel)game.getMap()).getMap(game.state)[1][4]);
+		game.moveHero('d'); // move hero to the right.
+		game.state = ((CustomLevel)game.getMap()).leversUp(game.state);
+		assertEquals(Game.Game_State.CLVL_KEY, game.state);
+		assertEquals('S', ((CustomLevel)game.getMap()).getMap(game.state)[1][4]);
+		game.moveHero('d'); // move hero to the right.
+		//game.updateGame('d');
+		game.moveHero('d'); // move hero to the right.
+		game.state = game.updateState();
+		assertEquals(Game.Game_State.WIN, game.state);
+	}
+	
+	@Test
+	public void testOgreMovesCustomLvl() {
+		
+		Game game = new Game();
+		char[][] temp = {
+				{'X','X','X','X','X','X','X'},
+				{'X','H',' ',' ',' ',' ','I'},
+				{'X',' ',' ',' ','O',' ','X'},
+				{'X',' ',' ',' ',' ',' ','X'},
+				{'X','X','X','X','X','X','X'}
+		};
+		game.initialize(0, 0, 0, temp);
+		assertEquals(Game.Game_State.CLVL, game.state);
+		//TODO
+		
+		//assertEquals(new Point(4,2), ((CustomLevel)game.getMap()).getOgres().get(0).getPoint());
+		game.updateGame('d');
+		//assertNotEquals(new Point(4,2), ((CustomLevel)game.getMap()).getOgres().get(0).getPoint());
+	}
+	
+	@Test
+	public void testDrawCustomLvl() {
+		
+		Game game = new Game();
+		char[][] temp = {
+				{'X','X','X','X','X','X','X'},
+				{'X','H',' ',' ',' ',' ','I'},
+				{'X',' ',' ',' ','O',' ','X'},
+				{'X',' ',' ',' ',' ',' ','X'},
+				{'X','X','X','X','X','X','X'}
+		};
+		game.initialize(0, 0, 0, temp);
+		//char[][] temp = game.drawScreen();
+	}
+	
+	
 	
 	
 }
