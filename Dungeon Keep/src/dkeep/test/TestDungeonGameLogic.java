73ba62b04e8bbda4814da17768fb6fcb1860c135
Fernando Alves <up201605270@fe.up.pt>
2018-04-05 14:13:21 +0100

    Started removing LevelTest JUnit tests

--- Dungeon Keep/src/dkeep/test/TestDungeonGameLogic.java
+++ Dungeon Keep/src/dkeep/test/TestDungeonGameLogic.java
@@ -10,72 +10,74 @@ public class TestDungeonGameLogic{
 	public void testMoveHeroIntoToFreeCell() {
 		
 		Game game = new Game();
-		game.setStateTest();
-		game.setPosHero(1, 1);
+		game.initialize(1, 0, 0, null);
 		assertEquals(new Point(1,1), game.getHeroPosition());
-		game.moveHero('s'); // move hero down.
-		assertEquals(new Point(1,2), game.getHeroPosition());
+		game.moveHero('d'); // move hero down
+		assertEquals(new Point(2,1), game.getHeroPosition());
 	}
-	
 	
 	@Test
 	public void testMoveHeroUp() {
 		
 		Game game = new Game();
-		game.setStateTest();
-		game.setPosHero(2, 2);
-		assertEquals(new Point(2,2), game.getHeroPosition());
+		game.initialize(1, 0, 0, null);
+		game.setPosHero(3, 2);
+		assertEquals(new Point(3,2), game.getHeroPosition());
 		game.moveHero('w'); // move hero up.
-		assertEquals(new Point(2,1), game.getHeroPosition());
+		assertEquals(new Point(3,1), game.getHeroPosition());
 	}
 	
 	@Test
 	public void testMoveHeroDown() {
 		
 		Game game = new Game();
-		game.setStateTest();
-		game.setPosHero(2, 2);
-		assertEquals(new Point(2,2), game.getHeroPosition());
-		game.moveHero('s'); // move hero down.
-		assertEquals(new Point(2,3), game.getHeroPosition());
+		game.initialize(1, 0, 0, null);
+		game.setPosHero(3, 2);
+		assertEquals(new Point(3,2), game.getHeroPosition());
+		game.moveHero('s'); // move hero up.
+		assertEquals(new Point(3,3), game.getHeroPosition());
 	}
 	
 	@Test
 	public void testMoveHeroRight() {
 		
 		Game game = new Game();
-		game.setStateTest();
-		game.setPosHero(2, 2);
-		assertEquals(new Point(2,2), game.getHeroPosition());
+		game.initialize(1, 0, 0, null);
+		game.setPosHero(1, 1);
+		assertEquals(new Point(1,1), game.getHeroPosition());
 		game.moveHero('d'); // move hero right.
-		assertEquals(new Point(3,2), game.getHeroPosition());
+		assertEquals(new Point(2,1), game.getHeroPosition());
 	}
 	
 	@Test
 	public void testMoveHeroLeft() {
 		
 		Game game = new Game();
-		game.setStateTest();
-		game.setPosHero(2, 2);
-		assertEquals(new Point(2,2), game.getHeroPosition());
+		game.initialize(1, 0, 0, null);
+		game.setPosHero(3, 1);
+		assertEquals(new Point(3,1), game.getHeroPosition());
 		game.moveHero('a'); // move hero left.
-		assertEquals(new Point(1,2), game.getHeroPosition());
+		assertEquals(new Point(2,1), game.getHeroPosition());
 	}
 	
 	@Test
-	public void testHeroIsCapturedByGuard() {
+	public void testMoveAgainstDoor_Wall() {
 		
 		Game game = new Game();
-		game.setStateTest();
-		((LevelTest)game.getMap()).guard.set_x(3);
-		((LevelTest)game.getMap()).guard.set_y(1);
-		game.setPosHero(1, 1);
-		assertFalse(game.isGameover());
-		game.moveHero('d'); // move hero to the right.
-		game.setState(game.collision());
-		assertTrue(game.isGameover());
+		game.initialize(1, 0, 0, null);
+		game.setPosHero(3, 1);
+		assertEquals(new Point(3,1), game.getHeroPosition());
+		game.moveHero('d'); // move hero right.
+		assertEquals(new Point(3,1), game.getHeroPosition());
+		game.moveHero('s'); // move hero right.
+		assertEquals(new Point(3,2), game.getHeroPosition());
+		game.moveHero('d'); // move hero right.
+		assertEquals(new Point(3,2), game.getHeroPosition());
 	}
+	
 	/*
+
+	
 	@Test
 	public void testHeroIsCapturedByOgre() {
 		
@@ -88,7 +90,6 @@ public class TestDungeonGameLogic{
 		game.setState(game.collision());
 		assertTrue(game.isGameover());
 	}
-*/
 	
 	@Test
 	public void testHeroStunsOgre() {
@@ -105,59 +106,12 @@ public class TestDungeonGameLogic{
 		assertEquals(2, ((LevelTest)game.getMap()).ogres.get(0).get_stun_count());
 	}
 
-	@Test
-	public void testKeyPicked() {
-		
-		Game game = new Game();
-		game.setStateTest();
-		game.setPosHero(1, 1);
-		assertFalse(game.isGameover());
-		game.moveHero('s'); // move hero downwards.
-		game.moveHero('s'); // move hero downwards.
-		game.setState(game.updateState());
-		game.getMap().leversUp(game.state);
-		assertEquals(new Point(1,3), game.getHeroPosition());
-		assertFalse(game.isGameover());
-		assertEquals(Game.Game_State.TEST_KEYPICKED, game.state);
-		game.moveHero('d'); // move hero right.
-		assertEquals(' ', game.getMap().getMap(game.state)[3][1]);
-	}
+
 	
-	@Test
-	public void testMoveAgainstWall() {
-		
-		Game game = new Game();
-		game.setStateTest();
-		game.setPosHero(1, 1);
-		game.moveHero('a'); // move hero left.
-		assertEquals(new Point(1,1), game.getHeroPosition());
-		game.moveHero('d'); // move hero right.
-		assertEquals(new Point(2,1), game.getHeroPosition());
-	}
+
 	
-	@Test
-	public void testMoveAgainstDoors() {
-		
-		Game game = new Game();
-		game.setStateTest();
-		char[][] temp = {
-				{'X','X','X','X','X'},
-				{'X',' ',' ',' ','I'},
-				{'X',' ',' ',' ','X'},
-				{'X',' ',' ',' ','S'},
-				{'X','X','X','X','X'}
-		};
-		
-		((LevelTest)game.getMap()).setTestMap(temp);
-		game.setPosHero(3, 1);
-		game.moveHero('d'); // move hero right.
-		assertEquals(new Point(3,1), game.getHeroPosition());
-		game.setPosHero(3, 3);
-		game.moveHero('d'); // move hero right.
-		assertEquals(new Point(4,3), game.getHeroPosition());
-		game.setState(game.updateState());
-		assertEquals(Game.Game_State.WIN, game.state);
-	}
+
+	*/
 	
 	@Test
 	public void testRookieMovement() {
